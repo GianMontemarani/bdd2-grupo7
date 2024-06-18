60da23d886337bc7d69a84f8a25b9156bd142785
DidierSaho <95487810+DidierSaho@users.noreply.github.com>
2024-06-18 19:14:56 -0300

    Update QHospital.sql
    Se agrego procedimientos de mati

--- QHospital.sql
+++ QHospital.sql
@@ -113,6 +113,285 @@ ON m.medico_id = d.medico_id
 where dia = 'lunes'
 order by m.nombre;
 
+-- PROCEDIMIENTOS ALMCENADO DE OPERACIONES CRUD
+DELIMITER 
+
+-- CREAR MEDICO!
+CREATE PROCEDURE CrearMedico (
+    IN p_nombre VARCHAR(80),
+    IN p_apellido VARCHAR(80),
+    IN p_especialidad VARCHAR(80)
+)
+BEGIN
+    INSERT INTO Medico (nombre, apellido, especialidad)
+    VALUES (p_nombre, p_apellido, p_especialidad);
+END //
+
+-- Leer Medico por ID
+CREATE PROCEDURE LeerMedico (IN p_medico_id INT)
+BEGIN
+    SELECT * FROM Medico WHERE medico_id = p_medico_id;
+END //
+
+-- Leer Todos los Medicos
+CREATE PROCEDURE LeerTodosMedicos ()
+BEGIN
+    SELECT * FROM Medico;
+END //
+
+-- Actualizar Medico
+CREATE PROCEDURE ActualizarMedico (
+    IN p_medico_id INT,
+    IN p_nombre VARCHAR(80),
+    IN p_apellido VARCHAR(80),
+    IN p_especialidad VARCHAR(80)
+)
+BEGIN
+    UPDATE Medico
+    SET nombre = p_nombre,
+        apellido = p_apellido,
+        especialidad = p_especialidad
+    WHERE medico_id = p_medico_id;
+END //
+
+-- Eliminar Medico
+CREATE PROCEDURE EliminarMedico (IN p_medico_id INT)
+BEGIN
+    DELETE FROM Medico WHERE medico_id = p_medico_id;
+END //
+
+DELIMITER ; 
+
+DELIMITER //
+
+-- CREAR DISPONIBILIDAD
+CREATE PROCEDURE CrearDisponibilidad (
+    IN p_dia ENUM('lunes','martes','miercoles','jueves','viernes','sabado','domingo'),
+    IN p_medico_id INT
+)
+BEGIN
+    INSERT INTO Disponibilidad (dia, medico_id)
+    VALUES (p_dia, p_medico_id);
+END //
+
+-- Leer Disponibilidad por ID
+CREATE PROCEDURE LeerDisponibilidad (IN p_disponibilidad_id INT)
+BEGIN
+    SELECT * FROM Disponibilidad WHERE disponibilidad_id = p_disponibilidad_id;
+END //
+
+-- Leer Todas las Disponibilidades
+CREATE PROCEDURE LeerTodasDisponibilidades ()
+BEGIN
+    SELECT * FROM Disponibilidad;
+END //
+
+-- Actualizar Disponibilidad
+CREATE PROCEDURE ActualizarDisponibilidad (
+    IN p_disponibilidad_id INT,
+    IN p_dia ENUM('lunes','martes','miercoles','jueves','viernes','sabado','domingo'),
+    IN p_medico_id INT
+)
+BEGIN
+    UPDATE Disponibilidad
+    SET dia = p_dia,
+        medico_id = p_medico_id
+    WHERE disponibilidad_id = p_disponibilidad_id;
+END //
+
+-- Eliminar Disponibilidad
+CREATE PROCEDURE EliminarDisponibilidad (IN p_disponibilidad_id INT)
+BEGIN
+    DELETE FROM Disponibilidad WHERE disponibilidad_id = p_disponibilidad_id;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+-- CREAR PACIENTE
+CREATE PROCEDURE CrearPaciente (
+    IN p_nombre VARCHAR(80),
+    IN p_apellido VARCHAR(80),
+    IN p_edad INT,
+    IN p_telefono VARCHAR(80),
+    IN p_contacto_emergencia VARCHAR(80)
+)
+BEGIN
+    INSERT INTO Paciente (nombre, apellido, edad, telefono, contacto_emergencia)
+    VALUES (p_nombre, p_apellido, p_edad, p_telefono, p_contacto_emergencia);
+END //
+
+-- Leer Paciente por ID
+CREATE PROCEDURE LeerPaciente (IN p_paciente_id INT)
+BEGIN
+    SELECT * FROM Paciente WHERE paciente_id = p_paciente_id;
+END //
+
+-- Leer Todos los Pacientes
+CREATE PROCEDURE LeerTodosPacientes ()
+BEGIN
+    SELECT * FROM Paciente;
+END //
+
+-- Actualizar Paciente
+CREATE PROCEDURE ActualizarPaciente (
+    IN p_paciente_id INT,
+    IN p_nombre VARCHAR(80),
+    IN p_apellido VARCHAR(80),
+    IN p_edad INT,
+    IN p_telefono VARCHAR(80),
+    IN p_contacto_emergencia VARCHAR(80)
+)
+BEGIN
+    UPDATE Paciente
+    SET nombre = p_nombre,
+        apellido = p_apellido,
+        edad = p_edad,
+        telefono = p_telefono,
+        contacto_emergencia = p_contacto_emergencia
+    WHERE paciente_id = p_paciente_id;
+END //
+
+-- Eliminar Paciente
+CREATE PROCEDURE EliminarPaciente (IN p_paciente_id INT)
+BEGIN
+    DELETE FROM Paciente WHERE paciente_id = p_paciente_id;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+-- Crear Cita
+DELIMITER //
+
+CREATE PROCEDURE CrearCita (
+    IN p_medico_id INT,
+    IN p_paciente_id INT,
+    IN p_estado ENUM('Programado', 'Cancelado'),
+    IN p_fecha DATE,
+    IN p_comentario VARCHAR(200)
+)
+BEGIN
+    -- Asignar valor por defecto a estado si es NULL
+    IF p_estado IS NULL THEN
+        SET p_estado = 'Programado';
+    END IF;
+
+    INSERT INTO Cita (medico_id, paciente_id, estado, fecha, comentario)
+    VALUES (p_medico_id, p_paciente_id, p_estado, p_fecha, p_comentario);
+END //
+
+DELIMITER ;
+DELIMITER //
+
+CREATE PROCEDURE LeerCita (
+    IN p_cita_id INT
+)
+BEGIN
+    SELECT * FROM Cita WHERE cita_id = p_cita_id;
+END //
+
+DELIMITER ;
+DELIMITER //
+
+CREATE PROCEDURE LeerTodasCitas ()
+BEGIN
+    SELECT * FROM Cita;
+END //
+
+DELIMITER ;
+DELIMITER //
+
+CREATE PROCEDURE ActualizarCita (
+    IN p_cita_id INT,
+    IN p_medico_id INT,
+    IN p_paciente_id INT,
+    IN p_estado ENUM('Programado', 'Cancelado'),
+    IN p_fecha DATE,
+    IN p_comentario VARCHAR(200)
+)
+BEGIN
+    -- Asignar valor por defecto a estado si es NULL
+    IF p_estado IS NULL THEN
+        SET p_estado = 'Programado';
+    END IF;
+
+    UPDATE Cita
+    SET medico_id = p_medico_id,
+        paciente_id = p_paciente_id,
+        estado = p_estado,
+        fecha = p_fecha,
+        comentario = p_comentario
+    WHERE cita_id = p_cita_id;
+END //
+
+DELIMITER ;
+DELIMITER //
+
+CREATE PROCEDURE EliminarCita (
+    IN p_cita_id INT
+)
+BEGIN
+    DELETE FROM Cita WHERE cita_id = p_cita_id;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+-- Crear Resultado
+CREATE PROCEDURE CrearResultado (
+    IN p_cita_id INT,
+    IN p_procedimiento TEXT,
+    IN p_recomendaciones TEXT,
+    IN p_medicacion TEXT
+)
+BEGIN
+    INSERT INTO Resultado (cita_id, procedimiento, recomendaciones, medicacion)
+    VALUES (p_cita_id, p_procedimiento, p_recomendaciones, p_medicacion);
+END //
+
+-- Leer Resultado por ID
+CREATE PROCEDURE LeerResultado (IN p_resultado_id INT)
+BEGIN
+    SELECT * FROM Resultado WHERE resultado_id = p_resultado_id;
+END //
+
+-- Leer Todos los Resultados
+CREATE PROCEDURE LeerTodosResultados ()
+BEGIN
+    SELECT * FROM Resultado;
+END //
+
+-- Actualizar Resultado
+CREATE PROCEDURE ActualizarResultado (
+    IN p_resultado_id INT,
+    IN p_cita_id INT,
+    IN p_procedimiento TEXT,
+    IN p_recomendaciones TEXT,
+    IN p_medicacion TEXT
+)
+BEGIN
+    UPDATE Resultado
+    SET cita_id = p_cita_id,
+        procedimiento = p_procedimiento,
+        recomendaciones = p_recomendaciones,
+        medicacion = p_medicacion
+    WHERE resultado_id = p_resultado_id;
+END //
+
+-- Eliminar Resultado
+CREATE PROCEDURE EliminarResultado (IN p_resultado_id INT)
+BEGIN
+    DELETE FROM Resultado WHERE resultado_id = p_resultado_id;
+END //
+
+DELIMITER ;
+
+
+
 
 
 
